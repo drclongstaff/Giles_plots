c662edfa2e94f98e9a0454d0a4242077289e2745
drclongstaff <drclongstaff@gmail.com>
2019-11-18 11:18:59 +0000

    Update Oct 7 CS640 anti HPA15aa Lab results stacked bar labels best.txt.R

--- Oct 7 CS640 anti HPA15aa Lab results stacked bar labels best.txt.R
+++ Oct 7 CS640 anti HPA15aa Lab results stacked bar labels best.txt.R
@@ -32,8 +32,8 @@ Res.DFc<-Res.DFc  %>% mutate(Labn_rev = rev(Lab_number)) # reverse the lab numbe
 
 #Write these files for checking
 #Check the data if necessary
-write.table(Res.DFc, "clipboard", sep="\t", col.names=T, row.names=F) 
-write.csv(Res.DFc, "Res.DF_plot.csv", row.names=F) 
+#write.table(Res.DFc, "clipboard", sep="\t", col.names=T, row.names=F) 
+#write.csv(Res.DFc, "Res.DF_plot.csv", row.names=F) 
 
 g_ghisc<-ggplot(Res.DFc, aes(Titre, fill = as.factor(Assay_number), group = nn))+
   scale_x_continuous(trans = 'log2', limits = c(0.7, 128), breaks = c(1,2,4,8,16,32, 64, 128), 
